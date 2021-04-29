fb2cbd55ccc3b21399e3e86d8fdbe51ccd54f5e8
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2021-04-29 16:51:05 -0300

    Add comandos

--- Manipulação de Arquivos/Anabelle.cpp
+++ Manipulação de Arquivos/Anabelle.cpp
@@ -3,215 +3,228 @@
 #include <locale.h>
 #include <string.h>
 
-struct RegisterFurniture{   
-    char name[200];
-    float value;
-    int cod;
-    char status;
+struct RegisterFurniture
+{
+	char name[200];
+	float value;
+	int cod;
+	char status;
 };
 
-FILE* OpenFile(char modo, char caminho[30]){
+FILE *OpenFile(char modo, char caminho[30])
+{
+	setlocale(LC_ALL, "Portuguese");
 	FILE *arquivo;
-	switch (modo){
-		case 'g' :
-			arquivo = fopen(caminho, "wt");	
-			break;
-			
-		case 'l':
-			arquivo = fopen(caminho, "rt");
-			break;
-			
-		case 'a':
-			arquivo = fopen(caminho, "a");
-			break;
-			
+	switch (modo)
+	{
+	case 'g':
+		arquivo = fopen(caminho, "wt");
+		break;
+
+	case 'l':
+		arquivo = fopen(caminho, "rt");
+		break;
+
+	case 'a':
+		arquivo = fopen(caminho, "a");
+		break;
 	}
-	
-	if(arquivo == NULL){
+
+	if (arquivo == NULL)
+	{
 		printf("Nao foi possivel abrir o arquivo");
 		exit(0);
 	}
-	
+
 	return arquivo;
 }
 
-//função de fechamento de arquivo
-void CloseFile(FILE *arquivo){
+//FunÃ§Ã£o de fechamento de arquivo
+void CloseFile(FILE *arquivo)
+{
 	fclose(arquivo);
 }
 
-//função de boas vindas
-void Start(){
+//FunÃ§Ã£o de boas vindas
+void Start()
+{
 	setlocale(LC_ALL, "Portuguese");
 	printf("\n\n\n\n\n\n\n\n\n\t\t\tBem vindo!\t\t\n\n\n\n\n\n\n\n\n\n\n\n");
-	
+
 	//system("pause");
-	//system("cls");
+	//system("clear");
 }
-//Função de busca
-void Search(){
-	system("cls");
-	FILE* arquivo;
-	
+//Funï¿½ï¿½o de busca
+void Search()
+{
+	system("clear");
+	FILE *arquivo;
+
 	arquivo = OpenFile('l', "database.txt");
-	
+
 	int cont = 0;
 	int searchcod;
 	struct RegisterFurniture Receive;
-	
+
 	setlocale(LC_ALL, "Portuguese");
-	printf("Insira o código do móvel desejado: ");
+	printf("Insira o cÃ³digo do mÃ³vel desejado: ");
 	scanf("%i", &searchcod);
-	setbuf(stdin,NULL);
-	
-		while(!feof(arquivo)){
-			
-			fscanf(arquivo, "%s %f %i %c ", &Receive.name, &Receive.value, &Receive.cod, &Receive.status);
-			if(Receive.cod == searchcod){
-				
-				if(Receive.status == 'd'){
-					printf("Nome: %s - Valor: R$ %.2f - Código: %i \n", Receive.name, Receive.value, Receive.cod);
-					cont = 1;
-				}
-					
-				else{
-					printf("Não Disponível");
-					cont = 1;
-				}
+	setbuf(stdin, NULL);
+
+	while (!feof(arquivo))
+	{
+
+		fscanf(arquivo, "%s %f %i %c ", &Receive.name, &Receive.value, &Receive.cod, &Receive.status);
+		if (Receive.cod == searchcod)
+		{
+
+			if (Receive.status == 'd')
+			{
+				printf("Nome: %s - Valor: R$ %.2f - CÃ³digo: %i \n", Receive.name, Receive.value, Receive.cod);
+				cont = 1;
 			}
+
+			else
+			{
+				printf("NÃ£o DisponÃ­vel");
+				cont = 1;
 			}
-				if(cont == 0){
-					printf("Não Encontrado");
-				}
-	
+		}
+	}
+	if (cont == 0)
+	{
+		printf("NÃ£o Encontrado");
+	}
+
 	CloseFile(arquivo);
-	
 }
 
-//Função de atualização
-//Não Funciona
-void Update(){
-	system("cls");
-	printf("\n\tEsta funcionalidade não esta disponível no momento!\n\tVisite https://github.com/WirisWernek/Atividades para futuras atualizações!\n");
-	printf("\n\n\tDesculpe, mas ainda não entendi como fazer isso!");
+//FunÃ§Ã£o de atualizaÃ§Ã£o
+//NÃ£o Funciona
+void Update()
+{
+	system("clear");
+	printf("\n\tEsta funcionalidade nÃ£o esta disponÃ­vel no momento!\n\tVisite https://github.com/WirisWernek/Atividades para futuras atualizaï¿½ï¿½es!\n");
+	printf("\n\n\tDesculpe, mas ainda nÃ£o entendi como fazer isso!");
 }
 
-//Função de remoção
-//Não Funciona
-void Remove(){
-	system("cls");
-	printf("\n\tEsta funcionalidade não esta disponível no momento!\n\tVisite https://github.com/WirisWernek/Atividades para futuras atualizações!\n");
-	printf("\n\n\tDesculpe, mas ainda não entendi como fazer isso!");
+//FunÃ§Ã£o de remoÃ§Ã£o
+//NÃ£o Funciona
+void Remove()
+{
+	system("clear");
+	printf("\n\tEsta funcionalidade nÃ£o esta disponÃ­vel no momento!\n\tVisite https://github.com/WirisWernek/Atividades para futuras atualizaÃ§Ãµes!\n");
+	printf("\n\n\tDesculpe, mas ainda nÃ£o entendi como fazer isso!");
 }
 
-//Função de cadastro
-void Register(){
-    FILE* arquivo;
-    struct RegisterFurniture Receive;
-	
+//FunÃ§Ã£o de cadastro
+void Register()
+{
+	FILE *arquivo;
+	struct RegisterFurniture Receive;
+
 	setlocale(LC_ALL, "Portuguese");
-	
-    arquivo = OpenFile('a', "database.txt");
-    
-    system("cls");
-    
-    printf("Insira o nome: ");
-    setbuf(stdin,NULL);
-    scanf("%s", &Receive.name);
 
-    printf("Insira o valor: ");
-    setbuf(stdin,NULL);
-    scanf("%f", &Receive.value);
+	arquivo = OpenFile('a', "database.txt");
 
-    printf("Insira o código: ");
-    setbuf(stdin,NULL);
-    scanf("%i", &Receive.cod);
+	system("clear");
 
-    printf("\n Foi registrado: \n Nome: %s Valor: R$ %.2f Código: %i ", Receive.name , Receive.value , Receive.cod);
-	Receive.status = 'd';    // 'd' para fixado na memória(disponível) e 'i' para pronto pra deletar(indisponível)
-	
+	printf("Insira o nome: ");
+	setbuf(stdin, NULL);
+	scanf("%s", &Receive.name);
+
+	printf("Insira o valor: ");
+	setbuf(stdin, NULL);
+	scanf("%f", &Receive.value);
+
+	printf("Insira o cÃ³digo: ");
+	setbuf(stdin, NULL);
+	scanf("%i", &Receive.cod);
+
+	printf("\n Foi registrado: \n Nome: %s Valor: R$ %.2f CÃ³digo: %i ", Receive.name, Receive.value, Receive.cod);
+	Receive.status = 'd'; // 'd' para fixado na memÃ³ria(disponÃ­vel) e 'i' para pronto pra deletar(indisponÃ­vel)
+
 	arquivo = OpenFile('a', "database.txt");
 	fprintf(arquivo, "%s %.2f %i %c \n", Receive.name, Receive.value, Receive.cod, Receive.status);
 	CloseFile(arquivo);
 }
 
-//Função de listagem
-void List(){
-	system("cls");
-	FILE* arquivo;
+//FunÃ§Ã£o de listagem
+void List()
+{
+	system("clear");
+	FILE *arquivo;
 	arquivo = OpenFile('l', "database.txt");
 	int cont = 0;
 	struct RegisterFurniture Receive;
-	
+
 	setlocale(LC_ALL, "Portuguese");
-	
-	printf("\t\tJá disponível: \n");
-		while(!feof(arquivo)){
-			
-			fscanf(arquivo, "%s %f %i %c ", &Receive.name, &Receive.value, &Receive.cod, &Receive.status);
-			
-			
-				if(Receive.status == 'd'){
-					printf("Nome: %s - Valor: R$ %.2f - Código: %i \n", Receive.name, Receive.value, Receive.cod);
-					}
-				}
-			CloseFile(arquivo);
-			}	
 
+	printf("\t\tJÃ¡ disponÃ­vel: \n");
+	while (!feof(arquivo))
+	{
 
+		fscanf(arquivo, "%s %f %i %c ", &Receive.name, &Receive.value, &Receive.cod, &Receive.status);
 
-int main(){
+		if (Receive.status == 'd')
+		{
+			printf("Nome: %s - Valor: R$ %.2f - CÃ³digo: %i \n", Receive.name, Receive.value, Receive.cod);
+		}
+	}
+	CloseFile(arquivo);
+}
+
+int main()
+{
 	char option;
 	int cont = 0;
 	int mode;
 	Start();
 	system("pause");
-	system("cls");
-	
-	printf("Já disponível:\n");
+	system("clear");
+
+	printf("JÃ¡ disponÃ­vel:\n");
 	List();
 	system("pause");
-	
-	do{
-	system("cls");
+
+	do
+	{
+		system("clear");
 		printf("\t----------MENU----------\t");
-		printf("\n\t1 - Registrar novo móvel");
-		printf("\n\t2 - Listar os móveis registrados");
-		printf("\n\t3 - Buscar um móvel");
-		printf("\n\t4 - Atualizar um móvel");
-		printf("\n\t5 - Deletar um móvel");
-		
-		printf("\n\nInsira o Valor da opção desejada: ");
+		printf("\n\t1 - Registrar novo mÃ³vel");
+		printf("\n\t2 - Listar os mÃ³veis registrados");
+		printf("\n\t3 - Buscar um mÃ³vel");
+		printf("\n\t4 - Atualizar um mÃ³vel");
+		printf("\n\t5 - Deletar um mÃ³vel");
+
+		printf("\n\nInsira o Valor da opÃ§Ã£o desejada: ");
 		scanf("%i", &mode);
-		switch (mode){
-			case 1:
-				Register();
-				cont++;
-				break;
-			case 2:
-				List();
-				break;
-			case 3:
-				Search();
-				break;
-			case 4:
-				Update();
-				break;
-			case 5:
-				Remove();
-				break;
-			default:
-				break;
-				
+		switch (mode)
+		{
+		case 1:
+			Register();
+			cont++;
+			break;
+		case 2:
+			List();
+			break;
+		case 3:
+			Search();
+			break;
+		case 4:
+			Update();
+			break;
+		case 5:
+			Remove();
+			break;
+		default:
+			break;
 		}
-	    
-	    
-	    
-	 
-	printf("\n\nDeseja continuar (S|N)? ");
-	setbuf(stdin,NULL);
-	scanf("%c", &option);
-	}while(option != 'n');
-   
+
+		printf("\n\nDeseja continuar (S|N)? ");
+		setbuf(stdin, NULL);
+		scanf("%c", &option);
+	} while (option != 'n');
+
 	return 0;
 }
