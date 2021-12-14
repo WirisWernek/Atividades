3739323b06df66b61909274981cb23bb1da7b588
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2021-12-13 21:23:29 -0300

    Adicionado função session

--- Web/ManipulacaoDeDadosComPHP/cadastrarpersonagem.php
+++ Web/ManipulacaoDeDadosComPHP/cadastrarpersonagem.php
@@ -1,20 +1,24 @@
 <?php
-$i = 0;
-$array[10];
-
-$nome = $_POST['nome'];
-$categoria = $_POST['categoria'];
-$hp = $_POST['hp'];
-$mana = $_POST['mana'];
-$forca = $_POST['forca'];
-$resistencia = $_POST['resistencia'];
-
-$array[$i] = ['nome' =>$nome, 'categoria' =>$categoria, 'hp' =>$hp, 'mana' =>$mana, 'forca' =>$forca, 'resistencia' =>$resistencia];
-$i++;
-for ($i=0; $i < 10; $i++) { 
-    echo "Nome: " . $array[$i]['nome'] . "<br>";
-}
-
- header('Location: ./index.html');
+    session_start();
+    $array;
+    $_SESSION['i'];
+    
+    if($_SESSION['i'] == NULL){
+        $_SESSION['i'] = 0;
+    }
+    
+    $nome = $_POST['nome'];
+    $categoria = $_POST['categoria'];
+    $hp = $_POST['hp'];
+    $mana = $_POST['mana'];
+    $forca = $_POST['forca'];
+    $resistencia = $_POST['resistencia'];
+    
+    $array = ['nome' =>$nome, 'categoria' =>$categoria, 'hp' =>$hp, 'mana' =>$mana, 'forca' =>$forca, 'resistencia' =>$resistencia];
+    
+    $_SESSION['array'][$_SESSION['i']] = $array;
+    $_SESSION['i']++;
+    
+    header('Location: ./index.html');
 
 ?>
--- Web/ManipulacaoDeDadosComPHP/personagens.php
+++ Web/ManipulacaoDeDadosComPHP/personagens.php
@@ -1,6 +1,7 @@
 <?php
-
-$descricao[] = [
+session_start();
+echo '<a href="./index.html">Inicio</a><br>';
+$descricao = [
     'Guerreiro'=>'O guerreiro é um personagem humano destinado à proteger seu rei não se importando como fará isso, ele tem grandes habilidades com armas de curto alcance, e tem vantagem contra Bruxos e desvantagem contra Caçadores', 
     
     'Mago'=>'O mago é um personagem humano que nasceu com o dom de manipular a magia para seus fins, são pácificos e normalmente preferem não se envolver em conflitos e guerras, mas podem destruir reinos e imperios se assim desejarem. Tem vantagem contra Druidas e desvantagem contra Elfos',
@@ -13,16 +14,56 @@ $descricao[] = [
 
     'Bruxo'=>'O bruxo é um personagem humano que consegue canaliza as forças das trevas, através de feitiços, encantamentos, rituais e objetos amaldiçoados. A alma dos bruxos é totalmente corrompida pelas forças das trevas, há quem diga que muitos deles nem conseguem mais controlar seus atos. Tem vantagem contra Assassinos e desvantagem contra Guerreiros e Druidas',
 
-    'Elfos'=>'Os Elfos são seres descendentes das fadas, tem uma enorme ligação com a natureza e ela atende à seus desejos, dedicam suas vidas à proteger a natureza e não se importam em morrer por isso. Odeiam Druidas por eles saberem de segredos que só os elfos deveriam saber. Tem vantagem contra Magos e desvantagem contra Paladinos',
+    'Elfo'=>'Os Elfos são seres descendentes das fadas, tem uma enorme ligação com a natureza e ela atende à seus desejos, dedicam suas vidas à proteger a natureza e não se importam em morrer por isso. Odeiam Druidas por eles saberem de segredos que só os elfos deveriam saber. Tem vantagem contra Magos e desvantagem contra Paladinos',
 
-    'Goblins'=>'Os Goblins são seres descendentes dos ogros, optam por viver nas profundezas das florestas de onde quem os imcomoda não costuma retornar. Tem uma força colossal e são muito bons com flechas e lanças. Tem vantagem contra Paladinos e desvantagem contra Druidas',
+    'Goblin'=>'Os Goblins são seres descendentes dos ogros, optam por viver nas profundezas das florestas de onde quem os imcomoda não costuma retornar. Tem uma força colossal e são muito bons com flechas e lanças. Tem vantagem contra Paladinos e desvantagem contra Druidas',
 
-    'Assassinos'=>'São as criaturas mais temidas, tem grandes conhecimentos em armas de curto e longo alcance, defesa contra magia e técnicas de camuflagem. São muito raros e silenciosos, tem uma aparência pálida, não se importam motivações nem justiça se importam apenas com seu dinheiro, não pagar o valor correto a um assassino é pior do que encomendar à propria morte para ele. Tem vantagem contra Caçadores e Magos e desvantagem contra Bruxos',
+    'Assassino'=>'São as criaturas mais temidas, tem grandes conhecimentos em armas de curto e longo alcance, defesa contra magia e técnicas de camuflagem. São muito raros e silenciosos, tem uma aparência pálida, não se importam motivações nem justiça se importam apenas com seu dinheiro, não pagar o valor correto a um assassino é pior do que encomendar à propria morte para ele. Tem vantagem contra Caçadores e Magos e desvantagem contra Bruxos',
 ];
 
 for ($i=0; $i < 10; $i++) { 
-    echo "Nome: " . $array[$i]['nome'] . "<br>";
+    if ($_SESSION['array'][$i] == NULL){
+        break;
+    }
+    echo "Nome: " . $_SESSION['array'][$i]['nome'] . "<br>";
+    echo "Categoria: " . $_SESSION['array'][$i]['categoria'] . "<br>";
+    echo "HP: " . $_SESSION['array'][$i]['hp'] . "<br>";
+    echo "Mana: " . $_SESSION['array'][$i]['mana'] . "<br>";
+    echo "Força: " . $_SESSION['array'][$i]['forca'] . "<br>";
+    echo "Resistencia: " . $_SESSION['array'][$i]['resistencia'] . "<br>";
+
+    switch ($_SESSION['array'][$i]['categoria']) {
+        case 'Guerreiro':
+            echo "Descrição: " . $descricao['Guerreiro'] . "<br>";
+            break;
+        case 'Mago':
+            echo "Descrição: " . $descricao['Mago'] . "<br>";
+            break;
+        case 'Paladino':
+            echo "Descrição: " . $descricao['Paladino'] . "<br>";
+            break;
+        case 'Druida':
+            echo "Descrição: " . $descricao['Druida'] . "<br>";
+            break;
+        case 'Caçador':
+            echo "Descrição: " . $descricao['Caçador'] . "<br>";
+            break;
+        case 'Bruxo':
+            echo "Descrição: " . $descricao['Bruxo'] . "<br>";
+            break;
+        case 'Elfo':
+            echo "Descrição: " . $descricao['Elfo'] . "<br>";
+            break;
+        case 'Goblin':
+            echo "Descrição: " . $descricao['Goblin'] . "<br>";
+            break;
+        case 'Assassino':
+            echo "Descrição: " . $descricao['Assassino'] . "<br>";
+            break;                                                                                          
+        default:
+            # code...
+            break;
+    }
+    echo "<hr>";
 }
-
-
 ?>
